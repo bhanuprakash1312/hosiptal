e039b77ebd40bd9e29b7334f0d4a93652c55f42a
bhanuprakash1312 <bp136897@gmail.com>
2026-05-02 14:30:58 +0530

    changed doctor section

--- server/app/routes/master.py
+++ server/app/routes/master.py
@@ -14,6 +14,16 @@ def get_departments(db: Session = Depends(get_db)):
 
 @router.get("/doctors/{department_id}")
 def get_doctors(department_id: int, db: Session = Depends(get_db)):
-    return db.query(Doctor).filter(
+    doctors = db.query(Doctor).filter(
         Doctor.department_id == department_id
     ).all()
+    
+    return [
+        {
+            "id": doc.id,
+            "name": doc.user.name,
+            "experience_years": 10,  # Mocked as it's not in DB
+            "consultation_fee": 500, # Mocked as it's not in DB
+            "department_id": doc.department_id
+        } for doc in doctors
+    ]
