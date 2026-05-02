3c3e4da7211dfda1bbca7df3bf5db599b9c1689d
bhanuprakash1312 <bp136897@gmail.com>
2026-05-02 15:59:04 +0530

    deploy

--- server/app/database.py
+++ server/app/database.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 #bhanu@1312#
 engine = create_engine(DATABASE_URL)
